2f2fd83897f7728d83d74069376433dc1a588d60
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-15 02:11:32 -0500

    Added air quality based colour changes -- #1

--- app.py
+++ app.py
@@ -4,20 +4,34 @@ import json
 
 root = Tk()
 root.title("Weath box")
-root.geometry("400x40")
+root.geometry("355x19")
 
 try:
-    api_req = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=F033E0BC-B84D-4531-85E3-FDADCF359CCF")
+    api_req = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=96122&distance=25&API_KEY=F033E0BC-B84D-4531-85E3-FDADCF359CCF")
     api = json.loads(api_req.content)
     city = api[0]['ReportingArea']
     quality = api[0]['AQI']
     category = api[0]['Category']['Name']
+
+    if category == "Good":
+        weather_colour = "green"
+    elif category == "Moderate":
+        weather_colour="Yellow"
+    elif category == "Unhealthy for sensitive groups":
+        weather_colour="Orange"
+    elif category == "Unhealthy":
+        weather_colour="Red"
+    elif category == "Hazardous":
+        weather_colour="Maroon"
+    
+    root.configure(bg="Purple")
+
+    api_label = Label(root, text=city + "'s Air quality = " + str(quality) + " -> " + category, font=(20),bg=weather_colour)  
+    api_label.pack()
+
 except Exception as e:
     api="Error"
     print(api)
-
-api_label = Label(root, text=city + "'s Air quality = " + str(quality) + " -> " + category, font=(20))
-api_label.pack()
 
 
 root.mainloop()
